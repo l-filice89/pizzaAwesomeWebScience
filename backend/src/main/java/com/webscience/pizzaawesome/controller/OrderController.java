7278cd6762d66856f3681b4b8558dc3911b85181
Luca Filice <l.filice.89@gmail.com>
2023-06-03 19:17:00 +0200

    Fixed CORS

--- backend/src/main/java/com/webscience/pizzaawesome/controller/OrderController.java
+++ backend/src/main/java/com/webscience/pizzaawesome/controller/OrderController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/orders")
 public class OrderController {
 
--- backend/src/main/java/com/webscience/pizzaawesome/controller/PizzaController.java
+++ backend/src/main/java/com/webscience/pizzaawesome/controller/PizzaController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/pizzas")
 public class PizzaController {
 
